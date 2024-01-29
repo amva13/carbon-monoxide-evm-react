b35d0388a084d62466619feda5bbe991aa1e80fd
Alejandro Velez <alex@alexv24.com>
2024-01-29 16:18:04 -0700

    web3 code and update submodule w/ mock

--- src/App.js
+++ src/App.js
@@ -1,7 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
 
+// web3
+const { Web3 } = require('web3');
+
 function App() {
+  // TODO: this is local ganache address
+  const contractAddress = ""
+
+  async function getContract() {
+    const {web3, accounts} = await getWeb3();
+    console.log("web3 is", web3)
+    // fetch artifact
+    const messageContractArtifact = require('./build/carbon-monoxide-evm/contracts/DiatomicMD');
+    const messageContractABI = messageContractArtifact.abi;
+    console.log(messageContractABI)
+    // instantiate contract object
+    console.log("getting account to append to contract")
+    const account = accounts[0]
+    console.log("account when getting contract is", account)
+    const messageContract = new web3.eth.Contract(messageContractABI, contractAddress, account);
+    return {messageContract: messageContract, web3: web3, accounts: accounts}
+  }
+
+  async function getWeb3() {
+    console.log("ran getWeb3")
+    // Wait for loading completion to avoid race conditions with web3 injection timing.
+    let web3
+    if (window.ethereum) {
+      web3 = new Web3(window.ethereum);
+      try {
+      console.log("ran new web3 method")
+        // Request account access if needed
+        await window.ethereum.enable();
+        // Accounts now exposed
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      // Use MetaMask/Mist's provider.
+      console.log("running legacy block for web3")
+      web3 = window.web3;
+      console.log('Injected web3 detected.');
+    }
+    // Fallback to localhost; use dev console port by default...
+    else {
+    console.log("running default local block for web3")
+      const provider = new Web3.providers.HttpProvider('http://127.0.0.1:9545');
+      web3 = new Web3(provider);
+      console.log('No web3 instance injected, using Local web3.');
+    }
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    console.log("retreived accounts", accounts)
+    return {web3: web3, accounts: accounts}
+};
+
+
   return (
     <div className="App">
       <header className="App-header">
