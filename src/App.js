4b494d21dea9a18bcaefa4088d314ce4d7a3fba9
Alejandro Velez <alex@alexv24.com>
2024-01-31 20:32:44 -0700

    can get output from smart contract

--- src/App.js
+++ src/App.js
@@ -6,21 +6,34 @@ const { Web3 } = require('web3');
 
 function App() {
   // TODO: this is local ganache address
-  const contractAddress = ""
+  const contractAddress = "0x34C4C006b6a8635697356df4876006658eC70506"
+
+  async function getSimOutput(){
+    console.log("getting contract")
+    const {messageContract, _} = await getContract()
+    console.log("the message contract is", messageContract)
+    const contract = messageContract
+    console.log("the contract is", contract)
+    console.log("finished awaiting getting contract")
+    // test get mock outputs
+    await contract.methods.getSimOutput().call().then(console.log)
+    console.log("finished call to getSimOutput")
+  }
 
   async function getContract() {
     const {web3, accounts} = await getWeb3();
     console.log("web3 is", web3)
     // fetch artifact
-    const messageContractArtifact = require('./build/carbon-monoxide-evm/contracts/DiatomicMD');
+    const messageContractArtifact = require('./carbon-monoxide-evm/build/contracts/DiatomicMD');
     const messageContractABI = messageContractArtifact.abi;
     console.log(messageContractABI)
     // instantiate contract object
-    console.log("getting account to append to contract")
-    const account = accounts[0]
-    console.log("account when getting contract is", account)
-    const messageContract = new web3.eth.Contract(messageContractABI, contractAddress, account);
-    return {messageContract: messageContract, web3: web3, accounts: accounts}
+    // console.log("getting account to append to contract")
+    // const account = accounts[0]
+    // console.log("account when getting contract is", account)
+    const messageContract = new web3.eth.Contract(messageContractABI, contractAddress, "0x34FAC91C4e7082C2f85ea6928DD7c21beeBBcfAb");
+    console.log("msg contract is", messageContract)
+    return {messageContract: messageContract, web3: web3}
   }
 
   async function getWeb3() {
@@ -53,8 +66,10 @@ function App() {
       web3 = new Web3(provider);
       console.log('No web3 instance injected, using Local web3.');
     }
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-    console.log("retreived accounts", accounts)
+    // accounts not needed here
+    // const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    const accounts = "" // TODO: change
+    // console.log("retreived accounts", accounts)
     return {web3: web3, accounts: accounts}
 };
 
@@ -74,6 +89,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={getSimOutput}>Click this for sim output.</button>
       </header>
     </div>
   );
